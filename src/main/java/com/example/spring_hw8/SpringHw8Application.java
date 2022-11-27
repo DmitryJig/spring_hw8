215d5c1bba623b4c802d462d3b85e116765deb9d
dima <jd2344430@gmail.com>
2022-11-27 22:37:25 +0500

    master_commit

---
+++ src/main/java/com/example/spring_hw8/SpringHw8Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_hw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHw8Application.class, args);
+	}
+
+}
